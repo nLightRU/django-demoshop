287ea1df185c1b6c91bc1ade006e5e893b2a5c53
Ilya <northenlight6899@gmail.com>
2024-03-05 21:12:05 +0300

    create model and initial view

--- demoshop/products/models.py
+++ demoshop/products/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
-# Create your models here.
-class Category(models.Model):
-    pass
-
-
-class Product(models.Model):
+class Brand(models.Model):
     name = models.CharField(max_length=120)
-    price = models.FloatField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Phone(models.Model):
+    phone_model = models.CharField(max_length=20, blank=True)
+    series = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=200000.00)
+
+    def clean(self):
+        if self.quantity < 0:
+            raise ValidationError("Amount can't be negative")
+        elif self.price < 0:
+            raise ValidationError("Price can't be negative")
+
+    def __str__(self) -> str:
+        return f'{self.series} {self.phone_model}'
--- demoshop/products/views.py
+++ demoshop/products/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from .models import Brand, Phone
 
-# Create your views here.
+
 def index(request):
     return HttpResponse('Hello from products')
+
+
+def phones(request):
+    phones_ = Phone.objects.all()
+    series = []
+    for phone in phones_:
+        series.append(phone.series)
+
+    resp = ', '.join(series)
+    return HttpResponse(resp)
